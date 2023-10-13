40e00a76e44207710768597e01334c7a2d1f640e
hackr25 <83638637+hackr25@users.noreply.github.com>
2023-10-13 14:35:51 +0530

    Add files via upload

---
+++ Oscilloscope.py
@@ -0,0 +1,81 @@
+"""
+steps:
+1.sudo raspi-config
+2.udo apt-get update
+sudo apt-get upgrade
+3.cd ~
+4.sudo apt-get install build-essential python-dev python-smbus git
+5.git clone https://github.com/adafruit/Adafruit_Python_ADS1x15.git
+6.cd Adafruit_Python_ADS1x15
+sudo python setup.py install
+7.cd examples
+8.python simpletest.py
+9.sudo apt-get install python-matplotlib
+10.sudo apt-get install python-pip12
+11.sudo pip install drawnow
+12.sudo nano scope.py
+13.import time
+import matplotlib.pyplot as plt
+from drawnow import *
+import Adafruit_ADS1x15
+14.adc = Adafruit_ADS1x15.ADS1115()
+15.GAIN = 1
+16.Val = [ ]
+cnt = 0
+17.plt.ion()
+18.adc.start_adc(0, gain=GAIN)
+19.plt.ylim(-5000,5000)
+plt.title('Osciloscope')
+plt.grid(True)
+plt.ylabel('ADC outputs')
+lt.plot(val, 'ro-', label='lux')
+plt.legend(loc='lower right')
+20.value = adc.get_last_result()
+21.print('Channel 0: {0}'.format(value))
+time.sleep(0.5)
+val.append(int(value))
+22.drawnow(makeFig)
+23.cnt = cnt+1
+if(cnt>50):
+val.pop(0)
+24.sudo python scope.py
+25.Import warnings
+import matplotlib.cbook
+warnings.filterwarnings(“ignore”, category=matplotlib.cbook.mplDeprecation)
+"""
+
+import time
+import matplotlib.pyplot as plt
+#import numpy
+from drawnow import *
+#import Adafruit_ADS1x15 module
+import Adafruit_ADS1x15
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
+GAIN = 1
+val = []
+cnt = 0
+plt.ion()
+# Start continuous ADC conversions on channel 0 using the previous gain value.
+adc.start_adc(0, gain=GAIN)
+print('Reading ADS1x15 channel 0')
+#create the figure function
+def makeFig():
+    plt.ylim(-5000,5000)
+    plt.title('Osciloscope')
+    plt.grid(True)
+    plt.ylabel('ADC outputs')
+    plt.plot(val, 'ro-', label='Channel 0')
+    plt.legend(loc='lower right')
+while (True):
+    # Read the last ADC conversion value and print it out.
+    value = adc.get_last_result()
+    print('Channel 0: {0}'.format(value))
+    # Sleep for half a second.
+    time.sleep(0.5)
+    val.append(int(value))
+    drawnow(makeFig)
+    plt.pause(.000001)
+    cnt = cnt+1
+    if(cnt>50):
+        val.pop(0)
---
+++ telegram.py
@@ -0,0 +1,62 @@
+"""
+steps:
+Step 1: Open Telegram app in your system or mobile
+Open Telegram app in your system or mobile\
+1.2 Start "BotFather
+1.3 Open "BotFather"
+1.4 Start "BotFather"
+/start
+1.5 Create a new Bot
+1.6 Obtain access token
+3.3 Install "Python Package Index"
+sudo apt-get install python-pip
+3.4 Install "telepot"
+sudo pip install telepot
+Step 4: Run the Python Code
+4.1 Clone the git
+git clone https://github.com/salmanfarisvp/TelegramBot.git
+4.2 Paste your Bot Token here
+bot = telepot.Bot('Bot Token')
+Note: 1.6 for more details
+4.3 Run the Code
+python telegrambot.py
+All set, now time to connect the Pi and LED.
+Step 5: Connect LED to Pi
+Step 6: Send Command
+6.1 Start our Bot
+6.2 Send "on" & "off"
+
+"""
+
+import sys
+import time
+import random
+import datetime
+import telepot
+import RPi.GPIO as GPIO
+#LED
+def on(pin):
+    GPIO.output(pin,GPIO.HIGH)
+    return
+def off(pin):
+    GPIO.output(pin,GPIO.LOW)
+    return
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+# set up GPIO output channel
+GPIO.setup(11, GPIO.OUT)
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    print 'Got command: %s' % command
+    if command == 'on':
+        bot.sendMessage(chat_id, on(11))
+    elif command =='off':
+        bot.sendMessage(chat_id, off(11))
+bot = telepot.Bot('Bot Token')#BotToken means that you have Generated from telegram
+bot.message_loop(handle)
+print 'I am listening...'
+while 1:
+    time.sleep(10)
+
+    
